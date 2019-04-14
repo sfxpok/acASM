b3ab810dff146d9e110021c8dae5c982e0888363
sfxpok <ricardo@airmail.cc>
2019-04-14 20:04:57 +0100

    added two menus

--- weighingmachine.asm
+++ weighingmachine.asm
@@ -5,5 +5,24 @@ OK                          EQU 0C0H    ; confirmation of the user input
 CHANGE                      EQU 0D0H    ; switch selection
 PESO                        EQU 0E0H    ; weight input of a food
 ; display
-display_start               EQU 20H     ; memory position to start the display
-display_end                 EQU 8FH     ; memory position to shut down the display
+DISPLAY_START               EQU 20H     ; memory position to start the display
+DISPLAY_END                 EQU 8FH     ; memory position to shut down the display
+; display menus
+PLACE 2000H
+
+InitMenu:
+  STRING "    WMachine    "
+  STRING "                "
+  STRING "1: Balanca      "
+  STRING "2: Registo      "
+  STRING "3: Visualizacao "
+  STRING "                "
+  STRING "                "
+ExceptionMenu:
+  STRING "                "
+  STRING "                "
+  STRING "Houve uma       "
+  STRING "excepcao        "
+  STRING "                "
+  STRING "                "
+  STRING "                "
