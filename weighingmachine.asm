49f73aecfc234b810a1e50c096d77dc4c9026f54
sfxpok <ricardo@airmail.cc>
2019-04-18 14:56:04 +0100

    added a couple of menus and routine placeholders

--- weighingmachine.asm
+++ weighingmachine.asm
@@ -6,23 +6,66 @@ CHANGE                      EQU 0D0H    ; switch selection
 PESO                        EQU 0E0H    ; weight input of a food
 ; display
 DISPLAY_START               EQU 20H     ; memory position to start the display
-DISPLAY_END                 EQU 8FH     ; memory position to shut down the display
+DISPLAY_END                 EQU 27H     ; memory position to shut down the display
 ; display menus
-PLACE 2000H
+PLACE 1000H
 
 InitMenu:
   STRING "    WMachine    "
   STRING "                "
   STRING "1: Balanca      "
-  STRING "2: Registo      "
-  STRING "3: Visualizacao "
+  STRING "2: Visualizacao "
+  STRING "3: Reiniciar    "
   STRING "                "
   STRING "                "
-ExceptionMenu:
+ErrorMenu:
   STRING "                "
   STRING "                "
-  STRING "Houve uma       "
-  STRING "excepcao        "
+  STRING "   Houve uma    "
+  STRING "   excepcao     "
+  STRING "                "
+  STRING "1: OK           "
+  STRING "                "
+ResetMenu:
+  STRING "                "
+  STRING "                "
+  STRING " Quer reiniciar "
+  STRING "   os dados?    "
+  STRING "                "
+  STRING "1: Sim          "
+  STRING "2: Nao          "
+weighMenu:
+  STRING "Alimento:       "
+  STRING "Peso:           "
   STRING "                "
   STRING "                "
   STRING "                "
+  STRING "                "
+  STRING "                "
+
+
+ChangeFoodMenu1:
+  STRING "Selec.Alim. P1/6"
+  STRING "                "
+  STRING "1: Aveia        "
+  STRING "2: Pao Forma    "
+  STRING "3: Batata       "
+  STRING "4: Arroz        "
+  STRING "0: Pagina 2     "
+;ChangeFoodMenu2:
+;ChangeFoodMenu3:
+;ChangeFoodMenu4:
+;ChangeFoodMenu5:
+;ChangeFoodMenu6:
+;ChangeFoodMenu7:
+
+PLACE 0000H
+Init:
+  MOV R0, InitMenu
+  JMP R0
+
+;roundGrams:
+;checkIfFoodIsSelected:
+;checkIfAboveMaxWeight:
+;calculateCalories:
+;wipeDisplay:
