71332efd2d4e1a3ceaf8677e629f69698c80df23
sfxpok <ricardo@airmail.cc>
2019-04-18 18:00:36 +0100

    main menu is somewhat done

--- weighingmachine.asm
+++ weighingmachine.asm
@@ -4,6 +4,10 @@ SEL_NR_MENU                 EQU 70H     ; selection input
 OK                          EQU 80H     ; confirmation of the user input
 CHANGE                      EQU 90H     ; switch selection
 PESO                        EQU 100H    ; weight input of a food
+; main menu constants
+WEIGHT_MACHINE EQU 1
+VIEW_FOOD EQU 2
+RESET_DATA EQU 3
 ; display
 DISPLAY_START               EQU 20H     ; memory position to start the display
 DISPLAY_END                 EQU 27H     ; memory position to shut down the display
@@ -64,16 +68,61 @@ Init:
   MOV R0, Startup
   JMP R0
 
+waitForPower:
+  PUSH R0
+  PUSH R1
+waitForPowerLoop:
+  MOV R0, PWR                           ; move PWR value to memory
+  MOVB R1, [R0]
+  CMP R1, 1                             ; is PWR set to 1?
+  JNE waitForPower                      ; PWR != 1?
+  POP R1
+  POP R0
+  RET
+
 Startup:
   MOV SP, StackPointer                  ; set stack pointer ready
 StartupLoop:
+  CALL waitForPower                     ; check if power is on
   CALL RMain                            ; display main menu
+  CALL IsOKActive
   JMP StartupLoop                       ; repeat routine
 
-RMain:
+
+errorMessage:
+
+main:
+  PUSH R0
+  PUSH R1
+  PUSH R2
+mainLoop:
   MOV R2, InitMenu                      ; get main menu ready
   CALL drawDisplay                      ; draw display
-
+  CALL wipePeripherals
+  CALL IsOKActive
+  MOV R0, SEL_NR_MENU                   ; move selection value to memory
+  MOVB R1, [R0]
+  CMP R1, WEIGHT_MACHINE
+  JEQ mainLoop_weighFood
+  CMP R1, VIEW_FOOD
+  JEQ mainLoop_viewFoodInfo
+  CMP R1, RESET_DATA
+  JEQ mainLoop_resetFoodData
+  CALL errorMessage
+  JMP mainLoop
+mainLoop_weighFood:
+  CALL weighFood
+  JMP mainLoop
+mainLoop_viewFoodInfo:
+  CALL viewFoodInfo
+  JMP mainLoop
+mainLoop_resetFoodData:
+  CALL resetFoodData
+  JMP mainLoop
+;mainLoop_end:
+;weighFood:
+;viewFoodInfo:
+;resetFoodData:
 ;roundGrams:
 ;checkIfFoodIsSelected:
 ;checkIfAboveMaxWeight:
@@ -84,6 +133,21 @@ drawDisplay:
   MOV R1, DISPLAY_END                   ; end of display
 ;checkIfPowerIsOn:
 ;drawDisplayLoop:
+IsOKActive:
+  PUSH R0
+  PUSH R1
+
+
+IsOKActiveLoop:
+  MOV R0, OK
+  MOVB R1, [R0]
+  CMP R1, 1
+  JNE IsOKActiveLoop
+  POP R1
+  POP R0
+  RET
+
+
 wipePeripherals:
   PUSH R0
   PUSH R1
