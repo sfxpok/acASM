c1ba298d3cd03d359ebc5c1f4e04a794e430a284
sfxpok <ricardo@airmail.cc>
2019-04-19 16:33:24 +0100

    fixed display routines and switched constants

--- weighingmachine.asm
+++ weighingmachine.asm
@@ -1,9 +1,9 @@
 ; peripherals
-PWR                         EQU 60H     ; start machine
-SEL_NR_MENU                 EQU 70H     ; selection input
-OK                          EQU 80H     ; confirmation of the user input
-CHANGE                      EQU 90H     ; switch selection
-PESO                        EQU 100H    ; weight input of a food
+PWR                         EQU 40H     ; start machine
+SEL_NR_MENU                 EQU 50H     ; selection input
+OK                          EQU 60H     ; confirmation of the user input
+CHANGE                      EQU 70H     ; switch selection
+PESO                        EQU 80H     ; weight input of a food
 ; stack pointer
 STACK_POINTER               EQU 9FF0H
 ; main menu constants
@@ -13,13 +13,14 @@ RESET_DATA                  EQU 3
 ; error menu constants
 GO_BACK                     EQU 1
 ; other constants
-MAX_WEIGHT                  EQU BB8H    ; 3000 in hexadecimal
-PROTEIN_CARB_MULTIPLICAND   EQU 4
-FAT_MULTIPLICAND            EQU 9
+MAX_WEIGHT                  EQU 3000    ; 3000 in hexadecimal
+PROTEIN_CARB_MULTIPLICAND   EQU 4       ; obtain protein and carbs in calories
+FAT_MULTIPLICAND            EQU 9       ; obtain fats in calories
+EMPTY_CHARACTER             EQU 20H
 
 ; display
-DISPLAY_START               EQU 20H     ; memory position to start the display
-DISPLAY_END                 EQU 27H     ; memory position to shut down the display
+DISPLAY_START               EQU 0A0H     ; position to start the display
+DISPLAY_END                 EQU 10FH     ; position to shut down the display
 
 ; reserved registers
 ; R10: weight of the food
@@ -76,12 +77,12 @@ ChangeFoodMenu1:
 ;ChangeFoodMenu6:
 ;ChangeFoodMenu7:
 
-PLACE 0000H
+PLACE 200H
 Init:
   MOV R0, Startup
   JMP R0
 
-PLACE 600H
+;PLACE 600H
 Startup:
   MOV SP, STACK_POINTER                 ; set stack pointer ready
 StartupLoop:
@@ -156,8 +157,10 @@ mainLoop_end:
 weighFood:
 viewFoodInfo:
 resetFoodData:
+
 ;roundGrams:
 ;checkIfFoodIsSelected:
+
 checkIfOverflow:
   PUSH R0
   ;PUSH R1
@@ -173,17 +176,18 @@ checkIfOverflow:
   POP R0
   RET
 
-
 checkIfAboveMaxWeight:
   PUSH R0
-  ;PUSH R1
+  PUSH R1
   MOV R0, [R10]                         ; weight of the food
-  CMP R0, MAX_WEIGHT
+  MOV R1, MAX_WEIGHT
+  CMP R0, R1
   JLT checkIfAboveMaxWeight_End
-  MOV [R10], 0                          ; weight is above 3000, therefore, set weight to 0
+  MOV R0, 0                             ; weight is above 3000, therefore, set weight to 0
+  MOV [R10], R0
 checkIfAboveMaxWeight_End:
   POP R0
-  ;POP R1
+  POP R1
   RET
 
 calculateCalories:
@@ -219,12 +223,31 @@ drawDisplay:
   MOV R0, DISPLAY_START                 ; beginning of display
   MOV R1, DISPLAY_END                   ; end of display
 drawDisplayLoop:
-  MOV R3, [R2]
-  MOV [R0], R3
-  ADD R2, 2
-  ADD R0, 2
+  MOV R3, [R2]                          ; get character from requested menu
+  MOV [R0], R3                          ; draw character to display
+  ADD R2, 2                             ; step to next char of menu
+  ADD R0, 2                             ; step to next char of display
   CMP R0, R1
-  JLE drawDisplayLoop
+  JLE drawDisplayLoop                   ; have we reached to the end of the menu?
+  POP R3
+  POP R2
+  POP R1
+  POP R0
+  RET
+
+wipeDisplay:
+  PUSH R0
+  PUSH R1
+  PUSH R2
+  PUSH R3
+  MOV R0, DISPLAY_START
+  MOV R1, DISPLAY_END
+  MOV R2, EMPTY_CHARACTER               ; this will be drawn all over the display
+wipeDisplayLoop:
+  MOVB [R0], R2                         ; send empty character to the display
+  ADD R0, 1                             ; step to next char of display
+  CMP R0, R1
+  JLE wipeDisplayLoop                   ; have we reached to the end of the menu?
   POP R3
   POP R2
   POP R1
@@ -235,10 +258,10 @@ IsCHANGEActive:
   PUSH R0
   PUSH R1
 IsCHANGEActiveLoop:
-  MOV R0, CHANGE
-  MOVB R1, [R0]
-  CMP R1, 1
-  JNE IsCHANGEActiveLoop
+  MOV R0, CHANGE                        ; move CHANGE value to register bank
+  MOVB R1, [R0]                         ; move CHANGE value to memory
+  CMP R1, 1                             ; is CHANGE pressed?
+  JNE IsCHANGEActiveLoop                ; CHANGE != 1?
   POP R1
   POP R0
   RET
@@ -247,10 +270,10 @@ IsOKActive:
   PUSH R0
   PUSH R1
 IsOKActiveLoop:
-  MOV R0, OK
-  MOVB R1, [R0]
-  CMP R1, 1
-  JNE IsOKActiveLoop
+  MOV R0, OK                            ; move OK value to register bank
+  MOVB R1, [R0]                         ; move OK value to memory
+  CMP R1, 1                             ; is OK pressed?
+  JNE IsOKActiveLoop                    ; OK != 1?
   POP R1
   POP R0
   RET
@@ -262,13 +285,13 @@ wipePeripherals:
   PUSH R3
   PUSH R4
   PUSH R5
-  MOV R0, PWR
+  ;MOV R0, PWR
   MOV R1, SEL_NR_MENU
   MOV R2, OK
   MOV R3, CHANGE
   MOV R4, PESO
   MOV R5, 0
-  MOVB [R0], R5                         ; wipe PWR input
+  ;MOVB [R0], R5                         ; wipe PWR input
   MOVB [R1], R5                         ; wipe selection input
   MOVB [R2], R5                         ; wipe OK input
   MOVB [R3], R5                         ; wipe CHANGE input
